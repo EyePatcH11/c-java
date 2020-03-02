61f9840ff3635ac16f2616c2997ea41dc832591b
EyePatcH11 <61144566+EyePatcH11@users.noreply.github.com>
2020-03-02 16:14:33 +0530

    Add files via upload

---
+++ 17th.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int counter;
+for(;;)
+{
+printf("%d\n",counter);
+}
+}
+
+
+
+
+
+
+
+
---
+++ 18th.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int counter=1;
+do
+{
+printf("hello\n");
+counter++;
+}while(counter<=0);
+}
+
+
+
+
+
+
+
+
